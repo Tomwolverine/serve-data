4f6d5504eb15c4b38f6e0a7f750b554f7bf22208
tom lee <tdroid99@gmail.com>
2018-07-31 18:01:44 -0600

    added heroku port

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 const cors = require('cors')
 const cohortData = require('./cohorts')
 
+app.use(express())
 app.use(cors())
 
 const findById = (params, data) => {
